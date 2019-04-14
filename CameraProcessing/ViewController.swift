e8a643f52fe70be880a3f65d6c622db678ac1e0d
Melhor <b05705046@ntu.edu.tw>
2019-04-14 10:56:49 +0800

    Initial Commit

---
+++ CameraProcessing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraProcessing
+//
+//  Created by dentall01 on 2019/4/14.
+//  Copyright © 2019 dentallio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
